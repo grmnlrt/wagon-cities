d3c2493d931ec9e75ee85f3d8c271f2c146c4fa7
Germain Loret <germain.loret@gmail.com>
2018-01-10 11:36:15 +0100

    try some tools

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { logger } from 'redux-logger';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
 
-import citiesReducer from './reducers/cities_reducer'
-import selectedCityReducer from './reducers/selected_city_reducer'
+import citiesReducer from './reducers/cities_reducer';
+import selectedCityReducer from './reducers/selected_city_reducer';
 
 // State and reducers
 const reducers = combineReducers({
@@ -18,9 +19,12 @@ const reducers = combineReducers({
   selectCity: selectedCityReducer
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(logger));
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
